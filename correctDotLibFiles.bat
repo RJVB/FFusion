6855202ccc014766412d37000302218ed7b1a2d8
RJVB <rjvbertin@gmail.com>
2013-01-29 12:31:04 +0100

    further reorganisation, and modification of the createStaticLibs-win32.sh script to build ffmpeg for win32 on Mac OS X. NB: script will be renamed! The script for building for Mac OS X is still the one from the Perian distribution.

---
+++ correctDotLibFiles.bat
@@ -0,0 +1,9 @@
+call "c:\Program Files (x86)\VS2010\VC\bin\vcvars32.bat"
+
+echo Correcting import .lib libraries
+
+lib.exe /verbose /machine:i386 /def:avcodec-52.def /out:avcodec-52.lib
+lib.exe /verbose /machine:i386 /def:avformat-52.def /out:avformat-52.lib
+lib.exe /verbose /machine:i386 /def:avdevice-52.def /out:avdevice-52.lib
+lib.exe /verbose /machine:i386 /def:avutil-50.def /out:avutil-50.lib
+lib.exe /verbose /machine:i386 /def:avcore-0.def /out:avcore-0.lib
